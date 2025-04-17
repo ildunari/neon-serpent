6f0c68f1aa2250789df0df71e3569264a5257ae7
ildunari <kosta963@gmail.com>
2025-04-17 03:56:35 -0400

    Refactor entity exports in index.js for clarity

--- src/entities/index.js
+++ src/entities/index.js
@@ -1,5 +1,6 @@
 // extracted from NeonSerpentGame_backup.jsx on 2025-04-17
 // Barrel file for entities
 export { default as Orb } from './Orb';
-export { default as Snake, { createAISnake } } from './Snake';
+export { default as Snake } from './Snake';
+export { createAISnake } from './Snake';
 export { default as Particle } from './Particle';
